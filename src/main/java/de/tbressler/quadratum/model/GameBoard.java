b37e3185024388df1e05d43ed6e26b4bc90c0988
tbressler <android.tbressler@gmail.com>
2016-10-30 12:15:11 +0100

    More methods added to game board model.

--- src/main/java/de/tbressler/quadratum/model/GameBoard.java
+++ src/main/java/de/tbressler/quadratum/model/GameBoard.java
@@ -1,5 +1,7 @@
 package de.tbressler.quadratum.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import static java.util.Objects.requireNonNull;
@@ -21,8 +23,14 @@ public class GameBoard {
     /* The active player. */
     private Player activePlayer;
 
+    /* The fields of the game board. */
+    private int[] board = new int[64];
+
     /* Is true if the game was started, otherwise false. */
     private boolean isStarted = false;
+
+    /* The game board listeners. */
+    private List<IGameBoardListener> listeners = new ArrayList<>();
 
 
     /**
@@ -34,18 +42,8 @@ public class GameBoard {
     public GameBoard(Player player1, Player player2) {
         if (Objects.equals(player1, player2))
             throw new AssertionError("player1 must not be equal to player2!");
-
         this.player1 = requireNonNull(player1);
         this.player2 = requireNonNull(player2);
-    }
-
-    /**
-     * Changes the active player to the given player.
-     *
-     * @param player The new active player.
-     */
-    private void setActivePlayerTo(Player player) {
-        this.activePlayer = player;
     }
 
     /**
@@ -76,6 +74,38 @@ public class GameBoard {
     }
 
     /**
+     * Starts the game. Clears the game board if a game was started before.
+     *
+     * @param activePlayer The active player, who can do the first turn. Must not be null.
+     */
+    public void startGame(Player activePlayer) {
+        checkActivePlayer(activePlayer);
+
+        clearGameBoard();
+        setActivePlayerTo(activePlayer);
+
+        isStarted = true;
+    }
+
+    /* Checks if the active player is valid. */
+    private void checkActivePlayer(Player activePlayer) {
+        if (!(requireNonNull(activePlayer).equals(player1) ||
+              requireNonNull(activePlayer).equals(player2)))
+            throw new AssertionError("Only player one or two can be set as active player!");
+    }
+
+    /** Clears the game board. */
+    private void clearGameBoard() {
+        for (int i = 0; i < 64; i++)
+            board[i] = 0;
+    }
+
+    /* Changes the active player to the given player. */
+    private void setActivePlayerTo(Player player) {
+        this.activePlayer = player;
+    }
+
+    /**
      * Returns true if the game was started, otherwise the method returns false.
      *
      * @return true if the game was started, otherwise false.
@@ -83,4 +113,23 @@ public class GameBoard {
     public boolean isStarted() {
         return isStarted;
     }
+
+    /**
+     * Adds a listener to the game board.
+     *
+     * @param listener the listener, must not be null.
+     */
+    public void addGameBoardListener(IGameBoardListener listener) {
+        listeners.add(requireNonNull(listener));
+    }
+
+    /**
+     * Removes a listener from the game board.
+     *
+     * @param listener the listener, must not be null.
+     */
+    public void removeGameBoardListener(IGameBoardListener listener) {
+        listeners.remove(requireNonNull(listener));
+    }
+
 }
--- src/test/java/de/tbressler/quadratum/model/TestGameBoard.java
+++ src/test/java/de/tbressler/quadratum/model/TestGameBoard.java
@@ -2,6 +2,7 @@ package de.tbressler.quadratum.model;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.internal.matchers.Null;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -77,6 +78,53 @@ public class TestGameBoard {
         assertEquals(null, gameBoard.getActivePlayer());
     }
 
+    /**
+     * Checks if getActivePlayer() returns player one after the game was started with player one
+     * as active player.
+     */
+    @Test
+    public void getActivePlayer_returnsPlayer1_ifGameIsStartedWithPlayer1() {
+        gameBoard.startGame(player1);
+        assertEquals(player1, gameBoard.getActivePlayer());
+    }
+
+    /**
+     * Checks if getActivePlayer() returns player two after the game was started with player two
+     * as active player.
+     */
+    @Test
+    public void getActivePlayer_returnsPlayer2_ifGameIsStartedWithPlayer2() {
+        gameBoard.startGame(player2);
+        assertEquals(player2, gameBoard.getActivePlayer());
+    }
+
+
+    /**
+     * Checks if an exception is thrown if the active player is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void startGame_withNull_throwsException() {
+        gameBoard.startGame(null);
+    }
+
+    /**
+     * Checks if an exception is thrown if the active player is not player one or two.
+     */
+    @Test(expected = AssertionError.class)
+    public void startGame_withUnknownPlayer_throwsException() {
+        gameBoard.startGame(mock(Player.class, "unknown-player"));
+    }
+
+    /**
+     * Checks if startGame() clears the game board.
+     */
+    @Test
+    public void startGame_clearsGameBoard() {
+
+        // TODO Check if game board is cleared after starting the game.
+
+    }
+
 
     /**
      * Checks if isStarted() returns false after the initialization of the game board, because
@@ -85,6 +133,27 @@ public class TestGameBoard {
     @Test
     public void isStarted_returnsFalse_afterNew() {
         assertEquals(false, gameBoard.isStarted());
+    }
+
+    /**
+     * Checks if isStarted() returns true after the game is started.
+     */
+    @Test
+    public void isStarted_returnsTrue_afterGameIsStarted() {
+        gameBoard.startGame(player1);
+        assertEquals(true, gameBoard.isStarted());
+    }
+
+
+    @Test(expected = NullPointerException.class)
+    public void addGameBoardListener_withNull_throwsException() {
+        gameBoard.addGameBoardListener(null);
+    }
+
+
+    @Test(expected = NullPointerException.class)
+    public void removeGameBoardListener_withNull_throwsException() {
+        gameBoard.removeGameBoardListener(null);
     }
 
 }
