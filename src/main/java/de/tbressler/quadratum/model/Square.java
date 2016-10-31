ef69e78813b82f7a77404b6d01aa09bc160ce977
tbressler <android.tbressler@gmail.com>
2016-10-31 14:40:29 +0100

    Added class for a square.

---
+++ src/main/java/de/tbressler/quadratum/model/Square.java
@@ -0,0 +1,13 @@
+package de.tbressler.quadratum.model;
+
+/**
+ * A square, which consists of 4 pieces of one player.
+ *
+ * @author Tobias Bressler
+ * @version 1.0
+ */
+public class Square {
+
+
+
+}
---
+++ src/test/java/de/tbressler/quadratum/model/TestSquare.java
@@ -0,0 +1,27 @@
+package de.tbressler.quadratum.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for the class Square.
+ *
+ * @author Tobias Bressler
+ * @version 1.0
+ */
+public class TestSquare {
+
+    // Class under test:
+    private Square square;
+
+
+    @Before
+    public void setUp() {
+        square = new Square();
+    }
+
+    @Test(expected = AssertionError.class)
+    public void new_withWrongFieldIndex1_throwsException() {
+        // TODO Test constructor.
+    }
+}
