69025061d72014898f970e27c7a81b705e783314
tbressler <android.tbressler@gmail.com>
2016-10-31 14:05:17 +0100

    Added JavaDoc.

--- src/main/java/de/tbressler/quadratum/model/IGameBoardListener.java
+++ src/main/java/de/tbressler/quadratum/model/IGameBoardListener.java
@@ -8,8 +8,19 @@ package de.tbressler.quadratum.model;
  */
 public interface IGameBoardListener {
 
+    /**
+     * This method is called, if the active player has changed.
+     *
+     * @param newPlayer The new player, who is now active.
+     */
     void onActivePlayerChanged(Player newPlayer);
 
+    /**
+     * This method is called, if a piece was placed on the game board.
+     *
+     * @param index The index where the piece was placed.
+     * @param player The player who has placed the piece.
+     */
     void onPiecePlaced(int index, Player player);
 
 }
