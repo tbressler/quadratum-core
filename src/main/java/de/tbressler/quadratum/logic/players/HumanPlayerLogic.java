5c5583fc36941ffaab937d8cb90e8bc09048cc12
tbressler <android.tbressler@gmail.com>
2016-11-06 11:20:06 +0100

    Implemented human player logic and tests.

--- src/main/java/de/tbressler/quadratum/logic/players/HumanPlayerLogic.java
+++ src/main/java/de/tbressler/quadratum/logic/players/HumanPlayerLogic.java
@@ -4,6 +4,11 @@ import de.tbressler.quadratum.logic.ILogicCallback;
 import de.tbressler.quadratum.model.IReadOnlyGameBoard;
 import de.tbressler.quadratum.model.Player;
 
+import java.util.concurrent.locks.ReentrantLock;
+
+import static com.google.common.collect.Range.closed;
+import static java.util.Objects.requireNonNull;
+
 /**
  * The player logic for a human player.
  *
@@ -11,6 +16,19 @@ import de.tbressler.quadratum.model.Player;
  * @version 1.0
  */
 public class HumanPlayerLogic extends AbstractPlayerLogic {
+
+    /* True if the player is active. */
+    private boolean isPlayerActive = false;
+
+    /* Lock. */
+    private ReentrantLock lock = new ReentrantLock(true);
+
+    /* The game board. */
+    private IReadOnlyGameBoard gameBoard = null;
+
+    /* The logic callback. */
+    private ILogicCallback logicCallback = null;
+
 
     /**
      * Creates the human player logic.
@@ -23,7 +41,53 @@ public class HumanPlayerLogic extends AbstractPlayerLogic {
 
     @Override
     public void requestMove(IReadOnlyGameBoard gameBoard, ILogicCallback callback) {
+        try {
+            lock.lock();
 
+            this.gameBoard = requireNonNull(gameBoard);
+            this.logicCallback = requireNonNull(callback);
+
+            isPlayerActive = true;
+
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
+    /**
+     * Returns true if the player is active or false if the opponent is active.
+     *
+     * @return True if the player is active.
+     */
+    public boolean isPlayerActive() {
+        return isPlayerActive;
+    }
+
+
+    /**
+     * Place a piece on the game board.
+     *
+     * @param index The field index, between 0..63.
+     * @return True if the piece was placed successfully.
+     */
+    public boolean placePiece(int index) {
+        if (!closed(0, 63).contains(index))
+            throw new AssertionError("Index must be between 0 and 63!");
+
+        try {
+            lock.lock();
+
+            if (!isPlayerActive)
+                return false;
+
+            isPlayerActive = false;
+
+            return logicCallback.makeMove(index, getPlayer());
+
+        } finally {
+            lock.unlock();
+        }
     }
 
 }
