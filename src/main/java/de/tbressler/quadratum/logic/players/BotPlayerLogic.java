c203062d29a4dca18c9f8c523453b37574368018
tbressler <android.tbressler@gmail.com>
2016-11-06 13:03:45 +0100

    Added more implementation to bot logic.

--- src/main/java/de/tbressler/quadratum/logic/players/BotPlayerLogic.java
+++ src/main/java/de/tbressler/quadratum/logic/players/BotPlayerLogic.java
@@ -7,6 +7,8 @@ import de.tbressler.quadratum.model.Player;
 import java.util.Random;
 
 import static com.google.common.base.MoreObjects.toStringHelper;
+import static de.tbressler.quadratum.utils.SquareUtils.getPossiblePieces;
+import static de.tbressler.quadratum.utils.SquareUtils.score;
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -59,8 +61,62 @@ public class BotPlayerLogic extends AbstractPlayerLogic {
         requireNonNull(gameBoard);
         requireNonNull(callback);
 
+        int[] playerHeatMap = new int[64];
+        int[] opponentHeatMap = new int[64];
 
+        Player[] pieces = new Player[4];
+        int[] possible;
 
+        int scoreForSquare;
+        int playerScore;
+        int opponentScore;
+        int numberOfPlayerPieces;
+        int numberOfOpponentPieces;
+
+        // Create heat maps for player and opponent:
+        for (int i = 0; i < 55; i++) {
+            pieces[0] = gameBoard.getPiece(i);
+            for (int j = i + 1; j < 64; j++) {
+                pieces[1] = gameBoard.getPiece(j);
+
+                possible = getPossiblePieces(i, j);
+                if (possible.length != 2)
+                    continue;
+
+                pieces[2] = gameBoard.getPiece(possible[0]);
+                pieces[3] = gameBoard.getPiece(possible[1]);
+
+                numberOfPlayerPieces = 0;
+                numberOfOpponentPieces = 0;
+
+                for (int p = 0; p < 4; p++)
+                    if (pieces[p] == getPlayer())
+                        numberOfPlayerPieces++;
+                    else if (pieces[p] != null)
+                        numberOfOpponentPieces++;
+
+                // Calculate possible score of square:
+                scoreForSquare = score(i, j, possible[0], possible[1]);
+                // ... add chance for opponent to get this square:
+                playerScore = scoreForSquare * numberOfPlayerPieces;
+                // ... add chance for player to get this square:
+                opponentScore = scoreForSquare * numberOfOpponentPieces;
+
+                if ((numberOfOpponentPieces > 0) && (numberOfPlayerPieces == 0)) {
+                   // ... square is not occupied by opponent and not yet blocked by player.
+                   opponentHeatMap[i] += opponentScore;
+                   opponentHeatMap[j] += opponentScore;
+                   opponentHeatMap[possible[0]] += opponentScore;
+                   opponentHeatMap[possible[1]] += opponentScore;
+                } else if (numberOfOpponentPieces == 0) {
+                   // ... square is not blocked by opponent.
+                   playerHeatMap[i] += playerScore;
+                   playerHeatMap[j] += playerScore;
+                   playerHeatMap[possible[0]] += playerScore;
+                   playerHeatMap[possible[1]] += playerScore;
+                }
+            }
+        }
     }
 
 
