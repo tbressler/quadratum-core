360d7ba91afce0f9ee39024296d7e0f16ee455c3
tbressler <android.tbressler@gmail.com>
2016-11-08 13:13:01 +0100

    Improved integration test.

--- src/test/java/de/tbressler/quadratum/IntTestLibrary.java
+++ src/test/java/de/tbressler/quadratum/IntTestLibrary.java
@@ -2,13 +2,11 @@ package de.tbressler.quadratum;
 
 import de.tbressler.quadratum.logic.GameLogic;
 import de.tbressler.quadratum.logic.IGameLogicListener;
-import de.tbressler.quadratum.logic.ILogicCallback;
 import de.tbressler.quadratum.logic.players.HumanPlayerLogic;
 import de.tbressler.quadratum.model.GameBoard;
 import de.tbressler.quadratum.model.IGameBoardListener;
 import de.tbressler.quadratum.model.Player;
 import de.tbressler.quadratum.model.Square;
-import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
@@ -33,7 +31,7 @@ public class IntTestLibrary {
     private IGameLogicListener logicListener = mock(IGameLogicListener.class, "logicListener");
 
     // Capture:
-    private ArgumentCaptor<Set> square = ArgumentCaptor.forClass(Set.class);
+    private ArgumentCaptor<Set> square = forClass(Set.class);
 
 
     @Test
@@ -72,23 +70,32 @@ public class IntTestLibrary {
             verify(boardListener, times(1)).onPiecePlaced(movesOfPlayer2[i], player2);
         }
 
+        // Check if listener notifications are correct:
         verify(logicListener, times(2)).onNewSquaresFound(eq(player1), square.capture());
         verify(logicListener, times(3)).onNewSquaresFound(eq(player2), square.capture());
-
         Set foundSquares = new HashSet();
         for (Set squareSet : square.getAllValues())
             foundSquares.addAll(squareSet);
+        assertSetOfSquares(foundSquares, player1, player2);
 
-        assertEquals(7, foundSquares.size());
+        // Check if game logic is correct:
+        Set squaresFromLogic = gameLogic.getSquares();
+        assertSetOfSquares(squaresFromLogic, player1, player2);
+    }
 
-        assertTrue(foundSquares.contains(new Square(new int[] { 1, 15, 48, 62}, player1)));
-        assertTrue(foundSquares.contains(new Square(new int[] {10, 12, 26, 28}, player1)));
-        assertTrue(foundSquares.contains(new Square(new int[] {12, 14, 28, 30}, player1)));
+    /* Assert if the set of squares contains the correct squares. */
+    private void assertSetOfSquares(Set squares, Player player1, Player player2) {
 
-        assertTrue(foundSquares.contains(new Square(new int[] {29, 34, 54, 59}, player2)));
-        assertTrue(foundSquares.contains(new Square(new int[] {36, 38, 52, 54}, player2)));
-        assertTrue(foundSquares.contains(new Square(new int[] {18, 20, 34, 36}, player2)));
-        assertTrue(foundSquares.contains(new Square(new int[] {20, 34, 38, 52}, player2)));
+        assertEquals(7, squares.size());
+
+        assertTrue(squares.contains(new Square(new int[] { 1, 15, 48, 62}, player1)));
+        assertTrue(squares.contains(new Square(new int[] {10, 12, 26, 28}, player1)));
+        assertTrue(squares.contains(new Square(new int[] {12, 14, 28, 30}, player1)));
+
+        assertTrue(squares.contains(new Square(new int[] {29, 34, 54, 59}, player2)));
+        assertTrue(squares.contains(new Square(new int[] {36, 38, 52, 54}, player2)));
+        assertTrue(squares.contains(new Square(new int[] {18, 20, 34, 36}, player2)));
+        assertTrue(squares.contains(new Square(new int[] {20, 34, 38, 52}, player2)));
     }
 
 }
